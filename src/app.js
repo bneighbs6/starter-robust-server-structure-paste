cfbaf7021d58e72cf10757c7990e86087add9105
Brett <brett_neighbors@yahoo.com>
2024-02-19 14:21:53 -0700

    Added app.post() to handle POST requests

--- src/app.js
+++ src/app.js
@@ -35,6 +35,27 @@ app.get("/pastes", (req, res) => {
   res.json({data: pastes});
 });
 
+// POST Request. ONLY used when making POST request to "/pastes"
+// Variable to hold the next ID
+// Because some IDs may already be used, find the largest assigned ID
+let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
+
+app.post("/pastes", (req, res, next) => {
+  const { data: { name, syntax, exposure, expiration, text, user_id } = {} } = req.body;
+  const newPaste = {
+    id: ++lastPasteId, // Increment last ID, then assign as the current ID
+    name,
+    syntax,
+    exposure,
+    expiration,
+    text,
+    user_id,
+  };
+  pastes.push(newPaste);
+  res.status(201).json({data: newPaste});
+});
+
+
 // Not found handler
 app.use((request, response, next) => {
   next(`Not found: ${request.originalUrl}`);
