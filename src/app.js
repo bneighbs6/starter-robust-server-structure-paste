0508911b8d956640d77f64a6b8b4c3454c69a580
Brett <brett_neighbors@yahoo.com>
2024-02-19 13:32:53 -0700

    End of Static Data Lesson

--- src/app.js
+++ src/app.js
@@ -2,10 +2,28 @@ const express = require("express");
 const app = express();
 
 // TODO: Follow instructions in the checkpoint to implement ths API.
-
 // Requiring paste data from ./data/pastes-data
 const pastes = require("./data/pastes-data");
-// Creating Route "/pastes" that will access paste data and return it to user.
+
+// Route for "/pastes/:pasteId".
+app.use("/pastes/:pasteId", (req, res, next) => {
+// Defines pasteId variable by destructuring it from req.params
+  const {pasteId} = req.params;
+// Use find() array method to search for the paste by Id.
+// If no id matches, find() returns undefined.
+  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
+
+// if foundPaste = truthy, then sends data with foundPaste object to the client as json. 
+  if (foundPaste) {
+    res.json({data: foundPaste});
+  } else {
+// Calls next with an error mesage to move request to error handler. 
+    next(`Paste id not found: ${pasteId}`);
+  }
+})
+
+// Creating Route "/pastes" that will access 
+// paste data and return it to user.
 app.use("/pastes", (req, res) => {
   res.json({data: pastes});
 });
