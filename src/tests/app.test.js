b40e304344c05b98b6e320a9ce4e60fc11baccc7
Brett <brett_neighbors@yahoo.com>
2024-02-20 14:57:16 -0700

    Created basic test setup in app.test.js. added supertest

---
+++ src/tests/app.test.js
@@ -0,0 +1,7 @@
+const request = require("supertest"); // Load SuperTest Library
+const pastes = require("../src/data/pastes-data"); // Load pastes data
+const app = require("..src/app"); // Load Express server
+
+describe("path /pastes", () => {
+    // Add tests here
+})
