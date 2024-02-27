07876d9ba67487cfb4b25c86823a320df5374cf8
Brett <brett_neighbors@yahoo.com>
2024-02-27 09:01:36 -0700

    Created methodNotAllowed.js. PUT requests do not return what I want

--- src/pastes/pastes.router.js
+++ src/pastes/pastes.router.js
@@ -1,15 +1,15 @@
 const router = require("express").Router(); // Creates new instance of Express router
 const controller = require("./pastes.controller"); // Importes the /pastes controller
-
-// using route() allows you to write the path once, and then chain multiple route handlers to that path. .get(controller.list) uses the list() route handler defined in the controller for GET requests to "/".
-router.route("/")
-.get(controller.list)
-.post(controller.create);  
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
 // new route for "/:pasteId" using the read fx from paste.controller
 router.route("/:pasteId")
 .get(controller.read) // Get request to read the paste
 .put(controller.update) // Put request to update the paste
-.delete(controller.delete); // Delete request to delete the paste
+.delete(controller.delete) // Delete request to delete the paste
+.all(methodNotAllowed); // Called at the end to be called only if no earlier handler completes request
+
+// using route() allows you to write the path once, and then chain multiple route handlers to that path. .get(controller.list) uses the list() route handler defined in the controller for GET requests to "/".
+router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed);  
 
 module.exports = router; 
