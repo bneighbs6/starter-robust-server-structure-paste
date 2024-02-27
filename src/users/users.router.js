9f4a9e1216d1c2ef19ef6630ada6fc65a3411ae8
Brett <brett_neighbors@yahoo.com>
2024-02-27 12:56:25 -0700

    Includes methodNotAllowed in users.router

--- src/users/users.router.js
+++ src/users/users.router.js
@@ -1,13 +1,14 @@
 // Create instance of Router()
 const router = require("express").Router();
 
+const methodNotAllowed = require("../errors/methodNotAllowed");
 // Import users.controller
 const controller = require("./users.controller");
 
 // Create route for "/:userId"
-router.route("/:userId").get(controller.read); // read() shows the specified user data from userId
+router.route("/:userId").get(controller.read).all(methodNotAllowed); // read() shows the specified user data from userId
 
 // Create route for "/"
-router.route("/").get(controller.list); // list() lists all of users data
+router.route("/").get(controller.list).all(methodNotAllowed); // list() lists all of users data
 
 module.exports = router;
