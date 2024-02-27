956586e942488544f11f6b9901bbf9ab656f6f06
Brett <brett_neighbors@yahoo.com>
2024-02-26 17:38:55 -0700

    Created and coded users.controller.js

---
+++ src/users/users.controller.js
@@ -0,0 +1,29 @@
+// Require users data
+const users = require("../data/users-data");
+
+// list() lists the users data
+function list(req, res) {
+    res.json({ data: users });
+}
+
+// Middleware validation fx to verify if user exists
+function userExists(req, res, next) {
+    const { userId } = req.params;
+    const foundUser = users.find((user) => user.id === Number(userId));
+    if (foundUser) {
+        res.locals.user = foundUser;
+        return next()
+    }
+    next({ status: 400, message: `User id not found ${userId}`, })
+}
+
+// read() reads the foundUser
+function read(req, res, next) {
+    res.json({ data: res.locals.user });
+}
+
+// Exports
+module.exports = {
+    list,
+    read: [userExists, read]
+}
