4b66a3b979e206d99e0553e1ce870a509789a8bc
Brett <brett_neighbors@yahoo.com>
2024-02-27 20:42:48 -0700

    Added pastesRouter to "/:userId/pastes"

--- src/users/users.router.js
+++ src/users/users.router.js
@@ -5,6 +5,10 @@ const methodNotAllowed = require("../errors/methodNotAllowed");
 // Import users.controller
 const controller = require("./users.controller");
 
+const pastesRouter = require("../pastes/pastes.router");
+
+router.use("/:userId/pastes", pastesRouter);
+
 // Create route for "/:userId"
 router.route("/:userId").get(controller.read).all(methodNotAllowed); // read() shows the specified user data from userId
 
