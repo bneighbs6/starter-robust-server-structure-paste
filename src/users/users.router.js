f30fe0b77a063e2092dd120628a2b6f17d4fb73d
Brett <brett_neighbors@yahoo.com>
2024-02-27 20:55:59 -0700

    Comments explaining the nested route "/:userId/pastes"

--- src/users/users.router.js
+++ src/users/users.router.js
@@ -1,12 +1,14 @@
-// Create instance of Router()
-const router = require("express").Router();
+// Create instance of Router(). mergeParams merges the parameters from parent route to child route.
+const router = require("express").Router({ mergeParams: true });
 
 const methodNotAllowed = require("../errors/methodNotAllowed");
 // Import users.controller
 const controller = require("./users.controller");
 
+// Imports pastes router.
 const pastesRouter = require("../pastes/pastes.router");
 
+// Nested Route. Attaching /pastes route to /users route
 router.use("/:userId/pastes", pastesRouter);
 
 // Create route for "/:userId"
