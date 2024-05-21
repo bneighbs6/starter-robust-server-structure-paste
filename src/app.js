c116f1e15489e5fa139d18cf92b63f442ee41052
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-05-20 21:41:47 -0600

    Moved router handler for :pasteid about /pastes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const app = express();
-const pastes = require("./data/pastes-data")
+const pastes = require("./data/pastes-data");
 
 // TODO: Follow instructions in the checkpoint to implement ths API.
 
-// /pastes path handler
+// "/pastes/:pasteId" path handler
+app.use("/pastes/:pasteId", (req, res, next) => {
+  const { pasteId } = req.params;
+  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
 
+  if (foundPaste) {
+    res.json({ data: foundPaste });
+  } else {
+    next(`Paste id not found: ${pasteId}`);
+  }
+});
+
+// "/pastes" path handler
 app.use("/pastes", (req, res, next) => {
-  res.json({ data: pastes })
-})
+  res.json({ data: pastes }); // Responds with the json data of the entire pastes array
+});
 
 // Not found handler
 app.use((request, response, next) => {
